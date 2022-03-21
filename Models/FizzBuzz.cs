0062b23e1f5dfbac64f60b39dc4c6b696b86061b
Łukasz Bielecki <37284@student.pb.edu.pl>
2022-03-21 09:48:58 +0100

    zad2

---
+++ Models/FizzBuzz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzzWeb.Models
+{
+    public class FizzBuzz
+    {
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        [Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}.")]
+        public int? Number { get; set; }
+
+
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,12 +5,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FizzBuzzWeb.Models;
 
 namespace FizzBuzzWeb.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+
+        
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,6 +27,27 @@ namespace FizzBuzzWeb.Pages
 
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if(FizzBuzz.Number%3==0 && FizzBuzz.Number%5!=0)
+                TempData["AlertMessage"] = "Fizz";
+            else if (FizzBuzz.Number % 5 == 0 && FizzBuzz.Number % 3 != 0)
+                TempData["AlertMessage"] = "Buzz";
+            else if (FizzBuzz.Number % 3 == 0 && FizzBuzz.Number % 5 == 0)
+                TempData["AlertMessage"] = "FizzBuzz";
+            else TempData["AlertMessage"] = "Liczba: <" + FizzBuzz.Number + "> nie spełnia kryteriów FizzBuzz";
+            return RedirectToPage("/Index");
 
         }
     }
